3d2be06c5e68b28bc11545b5f5f609c6c8f61746
Pedro Victor <pedro20002victor@gmail.com>
2022-08-25 23:26:44 -0300

    FIX: Erro 401 API

--- src/API/WeatherAPI.ts
+++ src/API/WeatherAPI.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const URL_API: string= `http://api.weatherapi.com/v1/current.json`;
-const KEY_API: string= `1123c83fa623413086e13551222608`
+const URL_API: string= 'http://api.weatherapi.com/v1/current.json';
+const KEY_API: string= 'key=ae2c15bdffd24a7eabb21343222608&'
 
 export const weatherApiRes = async (city: string) => {
    const { data } = await axios.get(URL_API, {
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
 import React, { useEffect, useState } from 'react';
+import { weatherApiRes } from './API/WeatherAPI';
 import { WeatherType } from './API/WeatherTypes';
 import './App.css';
 
 function App() {
   const [weather, setWeather] = useState({} as WeatherType);
-  const [city, setCity] = useState<string>();
+  const [city, setCity] = useState("");
   const [sucess, setSucess] = useState<boolean>(false);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if(city.trim()) {
+      try {
+        const data = await weatherApiRes(city);
+        setWeather(data);
+        setCity("");
+        setSucess(true);
+        console.log(`Pesquisa concluida com sucesso!`)
+      } catch(err) {
+        alert("Cidade nao encontrada!");
+        setSucess(false);
+      }
+    }
+
+  }
 
   return (
     <div className="App">
       <header className="App-header">
 
         <form 
-          action=""
-
+          onSubmit={handleSubmit}
         >
-          <input type="text" />
-          <button>Pesquisar</button>
+          <input 
+            type="text" 
+            name="city"
+            value={city}
+            onChange={e => setCity(e.target.value)}
+          />
+          <button
+            type='submit'
+          >Pesquisar</button>
         </form>
 
       </header>
